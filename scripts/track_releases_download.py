43688ead7f817096f707219882a8faa666ea5ff9
NorthblueM <northbluem@qq.com>
2025-03-24 22:12:26 +0800

    fix: bug, track_releases_download need reload df

--- scripts/track_releases_download.py
+++ scripts/track_releases_download.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import pandas as pd
-import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 import matplotlib
 import matplotlib.pyplot as plt
@@ -47,7 +47,7 @@ def process_release_data(releases, repo):
         tag_name = release['tag_name']
         release_name = release['name']
         release_date = release['published_at']
-        release_day = datetime.datetime.strptime(release_date, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
+        release_day = datetime.strptime(release_date, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
         
         one = [repo, tag_name, release_name, release_day]
 
@@ -93,21 +93,27 @@ def plot_downloads_repo(fpath, df):
     last_day = df['now_day'].max() # 获得最后一天的日期
     df_total = df[df['now_day']==last_day]
     
+    # 手绘风格
+    plt.xkcd(scale=1, length=100, randomness=2)
 
     # 设置画布, 两个子图, 左边history, 右边total
-    plt.xkcd(scale=1, length=100, randomness=2)
-    fig, axs = plt.subplots(1, 2, figsize=(13, 5))
+    fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+    # 间隔
+    # plt.subplots_adjust(wspace=0.25)
+
+    fs_title = 12
+    fs_ax = 10
 
     repo_name = df.iloc[0]['repo'].split('/')[1]
-    fig.suptitle(f'{repo_name} Releases Download Statistics', fontsize=16)
+    fig.suptitle(f'{repo_name} Releases Download Statistics', fontsize=fs_title)
 
     # # ========== history ==========
     ax = axs[0]
-    df_history.plot(x='now_day', y='tag_total', ax=ax)
-    ax.set_title('History')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Number of Downloads')
-    ax.legend(['Downloads'])
+    df_history.plot(x='now_day', y='tag_total', ax=ax, fontsize=fs_ax)
+    ax.set_title('History', fontsize=fs_ax+1)
+    ax.set_xlabel('Date', fontsize=fs_ax+1)
+    ax.set_ylabel('Number of Downloads', fontsize=fs_ax+1)
+    ax.legend(['Downloads'], fontsize=fs_ax)
     
     # 标注最后一个点
     # （#2c7fb8蓝、#2ca25f绿、#d62728红）
@@ -117,7 +123,7 @@ def plot_downloads_repo(fpath, df):
         xy=(last_point["now_day"], last_point["tag_total"]),
         xytext=(-70, 10),  # 偏移量
         textcoords='offset points',
-        fontsize=15,
+        fontsize=fs_ax+1,
         color='#d62728',
         arrowprops=dict(arrowstyle="->", color='#d62728', alpha=0.6)
     )
@@ -125,13 +131,24 @@ def plot_downloads_repo(fpath, df):
     # # ========== total ==========
     ax = axs[1]
     # 横坐标tag_name, 纵坐标tag_total
-    df_total.plot(x='tag_name', y='downloads', kind='bar', ax=ax)
-    ax.set_title('Total')
-    ax.set_xlabel('Version')
-    ax.set_ylabel('Number of Downloads')
-    ax.legend(['Downloads'])
+    df_total.plot(x='tag_name', y='downloads', kind='bar', ax=ax, fontsize=fs_ax)
+    ax.set_title('Total', fontsize=fs_ax+1)
+    ax.set_xlabel('Version', fontsize=fs_ax+1)
+    # ax.set_ylabel('Number of Downloads', fontsize=fs_ax+1)
+    ax.legend(['Downloads'], fontsize=fs_ax)
 
-    plt.savefig(fpath, bbox_inches='tight')
+    # 添加当前UTC时间到左下角
+    current_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
+    fig.text(
+        0.97, 0.97,  # 调整坐标以定位左上角
+        f'update: {current_time}',
+        fontsize=fs_ax-1,
+        color='gray',
+        ha='right',  # 水平右对齐
+        va='top'  # 垂直顶部对齐
+    )
+
+    plt.savefig(fpath, bbox_inches='tight', dpi=300)
     # plt.show()
 
 
@@ -153,7 +170,7 @@ def _main():
     fpath_svg = './data/releases_download_stats.svg'
 
     # 当前时间
-    now_day = datetime.datetime.now().strftime("%Y-%m-%d")
+    now_day = datetime.now().strftime("%Y-%m-%d")
 
     cols = ['now_day']
     cols.extend(['repo', 'tag_name', 'release_name', 'release_date', 'asset', 'downloads', 'tag_total'])
@@ -166,7 +183,8 @@ def _main():
 
     df = write_csv(fpath_csv, info, cols)
 
-    # df = pd.read_csv(fpath_csv)
+    # 仍需要重新读取，因为write_csv返回的只是增量
+    df = pd.read_csv(fpath_csv)
     
     plot_downloads(fpath_svg, df)
 
